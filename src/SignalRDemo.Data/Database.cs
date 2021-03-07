fd8593304189994290cd1fa456e9fe053ddf426e
georgy-kirilov <georgynk17@gmail.com>
2021-03-07 19:40:58 +0200

    Add SignalRDemo.Data

---
+++ src/SignalRDemo.Data/Database.cs
@@ -0,0 +1,20 @@
+﻿namespace SignalRDemo.Data
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Database
+    {
+        public bool DoesUserExist(string username, string password)
+        {
+            Dictionary<string, string> userPasswordPairs = File
+                    .ReadAllLines("/Resources/Users.txt")
+                    .Select(x => x.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                    .ToDictionary(x => x[0], x => x[1]);
+
+            return userPasswordPairs.ContainsKey(username) && userPasswordPairs[username] == password;
+        }
+    }
+}
